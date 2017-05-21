db68ff419bf5f095349435cc595e219762c7ccf9
umurtrky <umur.turkay@gmail.com>
2017-05-21 19:41:28 +0300

    resource handler added for images folder

--- sentwort/src/main/java/com/swe/dev/configuration/AppConfig.java
+++ sentwort/src/main/java/com/swe/dev/configuration/AppConfig.java
@@ -37,6 +37,7 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/styles/*").addResourceLocations("/styles/");
         registry.addResourceHandler("/js/*").addResourceLocations("/js/");
+        registry.addResourceHandler("/images/*").addResourceLocations("/images/");
     }
      
 
