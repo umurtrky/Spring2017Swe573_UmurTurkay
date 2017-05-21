cff14d6fd1e6bfc669d1f41f1f5a4b48fa69184e
umurtrky <umur.turkay@gmail.com>
2017-05-21 19:41:59 +0300

    a function to remove 4 byte characters

--- sentwort/src/main/java/com/swe/dev/converter/StatusMessageConverter.java
+++ sentwort/src/main/java/com/swe/dev/converter/StatusMessageConverter.java
@@ -7,13 +7,18 @@ import com.swe.dev.model.Message;
 import twitter4j.Status;
 
 public class StatusMessageConverter {
+	
+	public static String withNonBmpStripped( String input ) {
+	    if( input == null ) throw new IllegalArgumentException("input null");
+	    return input.replaceAll("[^\\u0000-\\uFFFF]", "");
+	}
 
 	public static Message convert(Status tweet){
 		Message message = new Message();
 		message.setId(String.valueOf(tweet.getId()));
 		message.setAccount(tweet.getUser().getScreenName());
         message.setLinks(tweet.getURLEntities().toString());
-        message.setMessage(tweet.getText());
+        message.setMessage(withNonBmpStripped(tweet.getText()));
         message.setSentiment(-1);
         message.setSharedate(tweet.getCreatedAt());
         message.setCreatedate(new Date());
